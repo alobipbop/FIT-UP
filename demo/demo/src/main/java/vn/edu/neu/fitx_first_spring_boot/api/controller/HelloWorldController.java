748936dd280dd43cc2fe2c511292d7575c4b2dcf
Tran Linh <linh20061210@gmail.com>
2026-02-07 10:54:03 +0700

    svv

--- demo/demo/src/main/java/vn/edu/neu/fitx_first_spring_boot/api/controller/HelloWorldController.java
+++ demo/demo/src/main/java/vn/edu/neu/fitx_first_spring_boot/api/controller/HelloWorldController.java
@@ -1,21 +1,23 @@
 package vn.edu.neu.fitx_first_spring_boot.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vn.edu.neu.fitx_first_spring_boot.api.service.HelloWorldService;
 
-@RestController
+@RestController //nơi request đi vào, bước 1-4
 @RequestMapping("/hello")
 public class HelloWorldController {
     HelloWorldService helloWorldService;
-
+    // duoi day la contructor
+    // sringboot giúp làm ngắn code, bỏ qua dependency, ko cần new helloWController
     public HelloWorldController(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
 
-    @GetMapping()
-    public String sayHello() {
-        return helloWorldService.sayHello();
+    @GetMapping("/hi") // => GET /hello/hi  -> tạo url
+    public String sayHello() {return helloWorldService.sayHello();
     }
+
 }
--- demo/demo/src/main/java/vn/edu/neu/fitx_first_spring_boot/api/service/HelloWorldService.java
+++ demo/demo/src/main/java/vn/edu/neu/fitx_first_spring_boot/api/service/HelloWorldService.java
@@ -1,11 +1,10 @@
 package vn.edu.neu.fitx_first_spring_boot.api.service;
 
 import org.springframework.stereotype.Service;
-
-@Service
+// tại sao phải tách service để xử lý: ở Pj lớn hơn, phải kết nối đến DB, bên thứ 3,... cần nhiều logic -> cần tách file service ra
+@Service //annotation trong Jv
 public class HelloWorldService {
     public String sayHello() {
-//        calculate something complex here
-        return "Hello, World!";
+        return "Hello, FitUp Spring Boot!";
     }
 }
